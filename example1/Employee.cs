6f1a650dda64af3bd38d199ef63593f33992fca6
Bethany Endersby <S00190486@mail.itsligo.ie>
2019-03-05 17:54:57 +0000

    examples from vid

---
+++ example1/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example1
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public DateTime JoinDate { get; set; }
+        public decimal WeeklyWage { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {JoinDate.ToShortDateString()} {WeeklyWage:C}";
+        }
+    }
+}
--- example1/MainWindow.xaml.cs
+++ example1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,67 @@ namespace example1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //startup code
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //string[] names = { "Tom", "Dick", "Harry" };
+
+            Employee e1 = new Employee() { Name = "Tom", JoinDate = new DateTime(2018, 01, 01), WeeklyWage = 500m };
+            Employee e2 = new Employee() { Name = "Dick", JoinDate = new DateTime(2018, 01, 01), WeeklyWage = 500m };
+            Employee e3 = new Employee() { Name = "Harry", JoinDate = new DateTime(2018, 01, 01), WeeklyWage = 500m };
+
+            //List<Employee> employees = new List<Employee>();
+
+            employees.Add(e1);
+            employees.Add(e2);
+            employees.Add(e3);
+
+            lbxEmployees.ItemsSource = employees;
+            tblkTotalWages.Text = GetTotalWages().ToString();
+        }
+
+        //add new employee
+        private void BtnAddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            //Employee emp = new Employee() { Name = "Mary", JoinDate = DateTime.Now, WeeklyWage = 500m };
+
+            //employees.Add(emp);
+
+            //lbxEmployees.ItemsSource = null;
+            //lbxEmployees.ItemsSource = employees;
+
+            //read info from screen
+            string name = tbxName.Text;
+            decimal wages = Convert.ToDecimal(tbxWages.Text);
+            DateTime joinDate = dpJoinDate.SelectedDate.Value;
+
+            //create object with info
+            Employee newEmployee = new Employee() { Name = name, JoinDate = joinDate, WeeklyWage = wages };
+
+            //add to collection
+            employees.Add(newEmployee);
+            tblkTotalWages.Text = GetTotalWages().ToString();
+
+
+        }
+
+        private decimal GetTotalWages()
+        {
+            decimal total = 0;
+
+            foreach (Employee e in employees)
+            {
+                total += e.WeeklyWage;
+            }
+
+            return total;
         }
     }
 }
